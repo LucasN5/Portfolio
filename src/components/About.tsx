82a80e2ea7eb104bd1c65c711aa290bd19bed80e
LucasN5 <lucasxben@hotmail.com>
2024-01-15 20:26:37 -0300

    About me structure update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,19 @@
 const about = () => {
   return (
-    <div>about</div>
+    <div className="main-container">
+      
+    <div className="">
+      <h1 className=""> Sobre mim</h1>
+    </div>
+
+      <div className="">
+        <img/>  
+      
+
+        <p className="">Apaixonado por tecnologia decidi seguir esta carreira da programação e encontrei minha paixão que é o front-end. Desenvolvo interfaces modernas e de alta qualidade.</p>
+      </div>
+
+    </div>
   )
 }
 
