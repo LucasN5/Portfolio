a77a1717899c951eebf5f7b2b76b85945bb82f4b
LucasN5 <lucasxben@hotmail.com>
2024-06-19 18:36:08 -0300

    Adiciona smooth scroll, efeito hover em navbar e foto nova de perfil

---
+++ src/js/animateScroll.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  for (const link of links) {
+    link.addEventListener("click", smoothScroll);
+  }
+
+  function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  }
+});
